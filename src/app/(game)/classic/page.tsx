40d97228e66ecb74c0e676a6cb5d4098262d4fa6
Andris Borbás <andris.borbas@gmail.com>
2023-04-19 15:18:47 +0200

    Add deterministic random generation

--- src/app/(game)/classic/page.tsx
+++ src/app/(game)/classic/page.tsx
@@ -1,9 +1,11 @@
 import { ClassicGame } from "@/components/games/Classic";
+import { getDeterministicRandom } from "@/lib/game";
 import { loadGods } from "@/lib/smiteApi";
 
 export default async function ClassicPage() {
 	const gods = await loadGods();
-	const actualGod = gods[Math.floor(Math.random() * gods.length)];
+	const random = getDeterministicRandom(new Date());
+	const actualGod = gods[Math.floor(random() * gods.length)];
 	// console.log(actualGod.Name);
 	return <ClassicGame gods={gods} actualGod={actualGod} />;
 }
---
+++ src/lib/game.ts
@@ -0,0 +1,16 @@
+import seedrandom from "seedrandom";
+
+/**
+ * The generator resets every day at 18:00 UTC+2 (Budapest time)
+ *
+ * Calling the return value generates the next random number, so only call it when you want a new number.
+ *
+ * @param date The date to use for the random number generation, should be the current date
+ */
+export function getDeterministicRandom(date: Date) {
+	const timeZone = "Europe/Budapest";
+	date.setHours(date.getHours() - 6);
+	const currentDate = date.toLocaleDateString("hu-HU", { timeZone });
+
+	return seedrandom(currentDate);
+}
