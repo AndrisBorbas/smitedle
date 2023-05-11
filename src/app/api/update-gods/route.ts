bd3615b02b7dcea14cb5d2964c0e14be7b145cad
Andris Borbás <andris.borbas@gmail.com>
2023-05-11 16:11:55 +0200

    Fix redirect error

--- src/app/api/update-gods/route.ts
+++ src/app/api/update-gods/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 
+import { siteConfig } from "@/data/site";
 import { getGods, saveGods, saveRawGods, trimGods } from "@/lib/smiteApi";
 import { dlog } from "@/lib/utils";
 
 export async function GET() {
 	if (process.env.NODE_ENV !== "development") {
-		return NextResponse.redirect("/");
+		return NextResponse.redirect(siteConfig.url);
 	}
 
 	const gods = await getGods();
